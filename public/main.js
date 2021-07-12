0f3a5fd1f6af284a262b6c5ea28107990d712302
Crystal Elsey <celsey@leoevents.com>
2021-07-12 13:51:33 -0500

    resized for smaller screens

--- public/main.js
+++ public/main.js
@@ -1,8 +1,7 @@
 const song = document.getElementById("sidepony");
-$("#text2").text("Click to open your gift!");
-$("#text2").css("color", "#41A990");
 
 $("#box").on("click", function () {
+  $("#text").css("display", "none");
   $("#box-lid").toggleClass("openedLid");
   song.play();
 
@@ -28,35 +27,37 @@ $("#box").on("click", function () {
     $("#newYork").toggleClass("openedNYC");
   }, 10500);
   setTimeout(function () {
-    $("#text2").text("Pack your bags! August 21st-25th");
-    $("#text2").css("color", "rgb(247, 246, 223)");
+    $("#text").css("display", "block");
+    $("#text").text("Pack your bags! August 21st-25th");
+    $("#text").css("color", "rgb(247, 246, 223)");
   }, 13250);
   setTimeout(function () {
-    $("#text2").css("display", "none");
+    $("#text").css("display", "none");
   }, 15250);
   setTimeout(function () {
-    $("#text2").css("display", "block");
+    $("#text").css("display", "block");
   }, 15750);
   setTimeout(function () {
-    $("#text2").css("display", "none");
+    $("#text").css("display", "none");
   }, 16750);
   setTimeout(function () {
-    $("#text2").css("display", "block");
+    $("#text").css("display", "block");
   }, 17250);
   setTimeout(function () {
-    $("#text2").css("display", "none");
+    $("#text").css("display", "none");
   }, 18250);
   setTimeout(function () {
-    $("#text2").css("display", "block");
+    $("#text").css("display", "block");
+    $("#text").text("Happy Birthday, Leah!");
   }, 18750);
   setTimeout(function () {
     $("#text").css("display", "block");
   }, 20500);
   setTimeout(function () {
-    $("#text2").css("display", "none");
+    $("#text").css("display", "none");
   }, 22500);
   setTimeout(function () {
-    $("#text2").css("display", "block");
-    $("#text2").text("From Shane, Crystal, and Rachel!");
+    $("#text").css("display", "block");
+    $("#text").text("From Shane, Crystal, and Rachel!");
   }, 22750);
 });
